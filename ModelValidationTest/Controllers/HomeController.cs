25848314d634a11fcabb37ececa759ccdf0ac9e0
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-06-16 11:22:28 +0400

    Add project files.

---
+++ ModelValidationTest/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ModelValidationTest.Models;
+using System.Diagnostics;
+
+namespace ModelValidationTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(EmployeeViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ViewBag.Error = "Invalid data";
+                return View();
+            }
+            
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ModelValidationTest/Models/EmployeeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ModelValidationTest.Models
+{
+    public class EmployeeViewModel
+    {
+        [Required]
+        public string Name { get; set; } = "";
+
+        public string? Surname { get; set; }
+
+        [MaxLength(10)]
+        [EmailAddress(ErrorMessage = "Es mail chi")]
+        public string Email { get; set; } = "";
+
+        public int? Age { get; set; }
+    }
+}
